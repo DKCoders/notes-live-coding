52adc52fe0e12e084024fbcd85156fb4db338256
Alexys Gonzalez <alexysbike@gmail.com>
2017-07-26 23:58:53 -0300

    Adding model Note and simple models loader

---
+++ api/api/models/note.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    title: {type: String, required: true},
+    content: {type: String, required: true},
+    labels: [{type: String, required: true}]
+});
+
+module.exports = mongoose.model('Note', noteSchema);
