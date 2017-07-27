b81787d539441b57fe1206d9d01b1886f74b01dd
Alexys Gonzalez <alexysbike@gmail.com>
2017-07-26 23:56:40 -0300

    Connecting with mongodb using mongoose

--- api/app.js
+++ api/app.js
@@ -1,24 +1,25 @@
-'use strict';
-
-var SwaggerRestify = require('swagger-restify-mw');
-var restify = require('restify');
-var app = restify.createServer();
-
+const SwaggerRestify = require('swagger-restify-mw');
+const restify = require('restify');
+const app = restify.createServer();
+const mongoose = require('mongoose');
+require('./api/models');
 module.exports = app; // for testing
 
-var config = {
-  appRoot: __dirname // required config
+const config = {
+    appRoot: __dirname // required config
 };
 
 SwaggerRestify.create(config, function(err, swaggerRestify) {
-  if (err) { throw err; }
+    if (err) { throw err; }
+    mongoose.connect('mongodb://localhost:27017');
+    mongoose.connection.once('open', () => {
+		console.log('Connection Established...');
+    });
+    swaggerRestify.register(app);
+    const port = process.env.PORT || 10010;
+    app.listen(port);
 
-  swaggerRestify.register(app);
-
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
-  if (swaggerRestify.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+    if (swaggerRestify.runner.swagger.paths['/hello']) {
+        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    }
 });
