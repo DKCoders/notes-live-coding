7a9bd124a47fc9e49c3c48156e00fdc655704c56
Alexys Gonzalez <alexysbike@gmail.com>
2017-07-27 00:45:50 -0300

    Adding GET method to retrieve one note by id

--- api/api/controllers/notes.js
+++ api/api/controllers/notes.js
@@ -9,7 +9,13 @@ const notesGet = async (req, res, next) => {
     const notes = await Note.find({});
     res.json({data: notes});
 }
+const notesGetById = async (req, res, next) => {
+    const noteId = req.swagger.params.noteId.value;
+    const note = await Note.findById(noteId);
+    res.json({data: note});
+};
 module.exports = {
     notesGet,
+    notesGetById,
     notesPost
 };
