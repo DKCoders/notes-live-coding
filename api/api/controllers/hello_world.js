a498ff1b617acbc00ad093db234f8da68caf4594
Alexys Gonzalez <alexysbike@gmail.com>
2017-07-26 23:29:17 -0300

    Upgrading swagger-restify-mw to 0.7.0.

--- api/api/controllers/hello_world.js
+++ api/api/controllers/hello_world.js
@@ -34,11 +34,11 @@ module.exports = {
   Param 1: a handle to the request object
   Param 2: a handle to the response object
  */
-function hello(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+function hello(req, res, next) {
   var name = req.swagger.params.name.value || 'stranger';
   var hello = util.format('Hello, %s!', name);
 
   // this sends back a JSON response which is a single string
   res.json(hello);
+  next()
 }
